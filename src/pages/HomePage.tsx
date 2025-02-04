bf06fad7de2e85bb3377905ea2ecebc0c99bbc45
marijustechin <marijus.smiginas@stud.techin.lt>
2025-02-04 14:19:05 +0400

    Startas

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+export const HomePage = () => {
+  return (
+    <main>
+      <h1>Redux pavyzdys</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure eum
+        accusantium dicta nam, ad eaque eveniet veniam atque iusto. Perspiciatis
+        quis nulla tempora magni quae dolore tempore, nostrum libero mollitia.
+      </p>
+    </main>
+  );
+};
