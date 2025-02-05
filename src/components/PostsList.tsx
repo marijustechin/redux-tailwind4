8786c58de7cd833bbbc1652831853c609e472310
Vaizdyla <m.smiginas@gmail.com>
2025-02-05 08:20:20 +0200

    Nereikšmingi pataisymai

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,14 +1,14 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router";
-import HelperService from "../services/HelperService";
-import { selectAllPosts } from "../store/features/posts/postsSlice";
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router';
+import HelperService from '../services/HelperService';
+import { selectAllPosts } from '../store/features/posts/postsSlice';
 
 export const PostsList = () => {
   const allPosts = useSelector(selectAllPosts);
 
   return (
     <div>
-      <div>
+      <div className="grid grid-cols-2 gap-2">
         {allPosts &&
           allPosts.map((post) => (
             <div key={post.title}>
@@ -16,7 +16,7 @@ export const PostsList = () => {
                 <h2>{HelperService.capitalizeFirstLetter(post.title)}</h2>
                 <img
                   className="rounded-lg"
-                  src={"https://picsum.photos/350/200/?blur=5"}
+                  src={'https://picsum.photos/350/200/?blur=5'}
                   alt={post.title}
                 />
                 <p>{HelperService.capitalizeFirstLetter(post.body)}</p>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
-import { PostsList } from "../components/PostsList";
+import { AddPostForm } from '../components/AddPostForm';
+import { PostsList } from '../components/PostsList';
 
 export const HomePage = () => {
   return (
     <main className="my-container">
       <h1>Redux pavyzdys</h1>
-      <PostsList />
+      <div className="grid grid-cols-2 gap-2">
+        <PostsList />
+        <AddPostForm />
+      </div>
     </main>
   );
 };
